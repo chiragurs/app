783aacb34844ffe52f6acb2f669d3a1e1b07ecd3
unknown <chiruurs1998@gmail.com>
2020-07-23 18:31:49 +0530

    initial commit

---
+++ chiru/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ChiruConfig(AppConfig):
+    name = 'chiru'
---
+++ chiru/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from math import factorial
+# Create your views here.
+def index(request):
+    return HttpResponse("<h1>welcome to app</h1>")
+
+def home(request):
+    return render(request,"chiru/home.html",{'name':'chirag urs m'})
+
+def fact(request,n):
+    n=int(n)
+    return HttpResponse("<h4>factorial is {}</h4>".format(factorial(n)))
